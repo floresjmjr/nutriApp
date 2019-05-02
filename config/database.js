712d4ecb6b94efe7ae2a9aa0834252a78839fc82
Jorge <jorge@Jorges-MacBook-Pro.local>
2019-05-02 16:57:47 -0400

    testing heroku 6

--- config/database.js
+++ config/database.js
@@ -2,15 +2,14 @@
 
 // Database
 const MongoClient = require('mongodb').MongoClient;
-const password = process.env.DATABASE_PASS
-const url = `mongodb+srv://floresjmjr:${password}@cluster0-iw5gt.mongodb.net/test?retryWrites=true`;
+// const url = `mongodb+srv://floresjmjr:${password}@cluster0-iw5gt.mongodb.net/test?retryWrites=true`;
 const mOptions = {useNewUrlParser: true}
 const dbName = 'test';
 
 var db = {};
 
 function connect() {
-  MongoClient.connect(url, mOptions, (err, client)=>{
+  MongoClient.connect(process.env.DATABASE_PASS, mOptions, (err, client)=>{
     if (err) { console.error('An error occurred connecting to database: ', err)
     } else {
       console.log('Connected successfully to server')
